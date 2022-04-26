2860a1c965ebf5f94c79cd81f726577a4681ab43
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-04-26 13:15:17 -0600

    day 2 start

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions and grab DOM elements
+const button = document.getElementById('diffbutton');
+document.getElementById();
+document.getElementById();
 
 // let state
+let win = 0;
+let losses = 0;
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+  button.addEventListener('click', () => {
+    win++; 
+    console.log(win);
+  });
+  
